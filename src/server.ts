6d0ba7234abd1b4acef8be3cd8aa7242c32c9496
leticea <nascimento.leticeah@gmail.com>
2025-03-25 09:44:55 -0300

    feat(server.ts): configuring the initial part of the routes

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello world");
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/test", () => {
+  return "Hello World!";
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server running!");
+});

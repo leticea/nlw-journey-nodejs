6544cd40215575938f388251d73c797bb8bd0614
leticea <nascimento.leticeah@gmail.com>
2025-03-25 10:39:26 -0300

    feat(server.ts): configuring the registration and listing route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import fastify from "fastify";
+import { prisma } from "./lib/prisma";
 
 const app = fastify();
 
-app.get("/test", () => {
-  return "Hello World!";
+app.get("/register", async () => {
+  await prisma.trip.create({
+    data: {
+      destination: "Salvador",
+      starts_at: new Date(),
+      ends_at: new Date(),
+    },
+  });
+
+  return "Successfully registered.";
+});
+
+app.get("/list", async () => {
+  const trips = await prisma.trip.findMany();
+
+  return trips;
 });
 
 app.listen({ port: 3333 }).then(() => {

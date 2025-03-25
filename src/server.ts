1762129302bbd13147ae34adbc5575f7325be46e
leticea <nascimento.leticeah@gmail.com>
2025-03-25 11:11:06 -0300

    feat(create-trip.ts): creating zod validation schema

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,17 @@
 import fastify from "fastify";
 import { prisma } from "./lib/prisma";
+import { createTrip } from "./routes/create-trip";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
 
 const app = fastify();
 
-app.get("/register", async () => {
-  await prisma.trip.create({
-    data: {
-      destination: "Salvador",
-      starts_at: new Date(),
-      ends_at: new Date(),
-    },
-  });
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-  return "Successfully registered.";
-});
-
-app.get("/list", async () => {
-  const trips = await prisma.trip.findMany();
-
-  return trips;
-});
+app.register(createTrip);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running!");

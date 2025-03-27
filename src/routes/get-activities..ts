64475f62cbf46f51f37f439376424fd8c67501fc
leticea <nascimento.leticeah@gmail.com>
2025-03-27 16:42:43 -0300

    feat(get-activities.ts): creating the list of activities, separating each one by date

--- src/routes/get-activities..ts
+++ src/routes/get-activities..ts
@@ -22,7 +22,11 @@ export async function getActivities(app: FastifyInstance) {
           id: tripId,
         },
         include: {
-          activities: true,
+          activities: {
+            orderBy: {
+              occurs_at: "asc",
+            },
+          },
         },
       });
 
@@ -30,7 +34,25 @@ export async function getActivities(app: FastifyInstance) {
         throw new Error("Trip not found.");
       }
 
-      return { activities: trip.activities };
+      const differenceInDaysBetweenTripStartAndEnd = dayjs(trip.ends_at).diff(
+        trip.starts_at,
+        "days"
+      );
+
+      const activities = Array.from({
+        length: differenceInDaysBetweenTripStartAndEnd + 1,
+      }).map((_, index) => {
+        const date = dayjs(trip.starts_at).add(index, "days");
+
+        return {
+          date: date.toDate(),
+          activities: trip.activities.filter((activity) => {
+            return dayjs(activity.occurs_at).isSame(date, "day");
+          }),
+        };
+      });
+
+      return { activities };
     }
   );
 }

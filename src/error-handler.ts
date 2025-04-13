a7a4318a953d7d81b3e169e2300e7ec8a13eab07
leticea <nascimento.leticeah@gmail.com>
2025-04-13 11:52:54 -0300

    feat(error-handler.ts): dealing with status 400 server error

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,9 +1,17 @@
 import type { FastifyInstance } from "fastify";
 import { ClientError } from "./errors/client-error";
+import { ZodError } from "zod";
 
 type FastifyErrorHandler = FastifyInstance["errorHandler"];
 
 export const errorHandler: FastifyErrorHandler = (error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: "Invalid input",
+      errors: error.flatten().fieldErrors,
+    });
+  }
+
   if (error instanceof ClientError) {
     return reply.status(400).send({
       message: error.message,

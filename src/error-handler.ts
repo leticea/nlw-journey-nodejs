8e3f2728ba3e3e3ed7cf9ea6572186beb6216ce8
leticea <nascimento.leticeah@gmail.com>
2025-04-13 11:17:26 -0300

    feat(error-handler.ts): dealing with status 400 server error

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,9 +1,14 @@
-import { FastifyInstance } from "fastify";
+import type { FastifyInstance } from "fastify";
+import { ClientError } from "./errors/client-error";
 
 type FastifyErrorHandler = FastifyInstance["errorHandler"];
 
 export const errorHandler: FastifyErrorHandler = (error, request, reply) => {
-  console.log(error);
+  if (error instanceof ClientError) {
+    return reply.status(400).send({
+      message: error.message,
+    });
+  }
 
-  return reply.status(500).send({ message: "Internal server error " });
+  return reply.status(500).send({ message: "Internal server error" });
 };
